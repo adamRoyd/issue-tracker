bc4ff772bebec6f4ff448d20db6c5c047e6f88a3
Adam Boothroyd BW <adam.boothroyd@brightwavegroup.com>
2017-04-04 13:42:59 +0100

    Added ToDoList example

--- issue-tracker-app/src/index.js
+++ issue-tracker-app/src/index.js
@@ -1,2 +1,15 @@
-/* eslint-disable no-console */
-console.log('hi');
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import todoApp from './reducers/index';
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
