61f3881f15ecc8803d1ae6b302e8ee2ddce6a3d9
Adam Boothroyd BW <adam.boothroyd@brightwavegroup.com>
2017-04-07 16:50:50 +0100

    installed babel preset stage 2 / added onclock class to Issue row

--- issue-tracker-app/src/components/Issue.js
+++ issue-tracker-app/src/components/Issue.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class Issue extends React.Component{
+    rowSelect(){
+        console.log('CHANGE SELECTED CLASS');
+    }
     render(){
+        this.rowSelect = this.rowSelect.bind(this);
         const { issue } = this.props;
         return(
-            <tr>
+            <tr onClick={this.rowSelect}>
                 <td><Link to={`/issue/${issue.id}`} activeClassName="active">{issue.id}</Link></td>
                 <td>{issue.screen}</td>
                 <td>{issue.category}</td>
