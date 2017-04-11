9a5218d1c655abf278b04a13e19f0959a271598d
Adam Boothroyd BW <adam.boothroyd@brightwavegroup.com>
2017-04-11 17:17:12 +0100

    Added DropZone to form

--- issue-tracker-app/src/components/CommentForm.js
+++ issue-tracker-app/src/components/CommentForm.js
@@ -1,7 +1,19 @@
 import React,{PropTypes} from 'react';
 import Dropdown from '../common/Dropdown';
+import DropZone from 'react-dropzone';
 
 class CommentForm extends React.Component{
+
+    onDrop(files){
+        console.log('Recieved files :', files);
+        this.setState({
+            files: files
+        });
+    }
+
+    onOpenClick() {
+        this.refs.dropzone.open();
+    }
 
     handleSubmit(e){
         e.preventDefault();
@@ -14,20 +26,33 @@ class CommentForm extends React.Component{
     }
     render(){
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onOpenClick = this.onOpenClick.bind(this);
         return(
             <div>
-                <form className="form-horizontal" ref="commentForm" onSubmit={this.handleSubmit}>
-                    <textarea type="text" className="col-sm-7" rows="3" ref="comment" placeholder="comment"/>
-                    <div className="col-sm-5">
-                        <select ref="assigned" className="form-control">
-                            <option value="adam">adam</option>
-                        </select>
-                        <Dropdown ref="status" options={this.props.status}/>
-                    </div>
-                    <input className="btn" type="submit"/>  
-                    <button id="attach" className="btn">Add attachment</button>
-                </form>
-                
+                <DropZone ref="dropzone" className="DropZone" activeClassName="DropZoneOver" disableClick="true" onDrop={this.onDrop}>
+                    <form className="form-horizontal" ref="commentForm" onSubmit={this.handleSubmit}>
+                    
+                            <textarea type="text" className="col-sm-7" rows="3" ref="comment" placeholder="comment"/>
+                    
+                        <div className="col-sm-5">
+                            <select ref="assigned" className="form-control">
+                                <option value="adam">adam</option>
+                            </select>
+                            <Dropdown ref="status" options={this.props.status}/>
+                        </div>
+                        <input className="btn" type="submit"/>  
+                        <button id="attach" className="btn" onClick={this.onOpenClick}>Add attachment</button>
+                        
+                    </form>
+               </DropZone>  
+                {/*{this.state.files ? 
+                    <div>
+                        <h2>Uploading {files.length} files...</h2>
+                        <div>{this.state.files.map((file) => <img src={file.preview} />)}</div>
+                    </div> 
+                    : 
+                    <p>hi</p>
+                }*/}
             </div>
         );
     }
