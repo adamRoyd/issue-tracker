da4279d75231a28ac88ce37a271a317e3d9f320d
Adam Boothroyd BW <adam.boothroyd@brightwavegroup.com>
2017-06-12 16:25:28 +0100

    File loader

--- issue-tracker-mern/client/components/Issue/IssueDescription.js
+++ issue-tracker-mern/client/components/Issue/IssueDescription.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import img from '../../../dist/test.jpg'
+//import img from '../../../test.png'
+var url = require("file-loader!../../../uploads/test.png");
 
 class IssueDescription extends React.Component{
     render(){
+        console.log(url);
         return(
             <div className="col-sm-7">
                 <p>{this.props.issue.description}</p>
-                {/*<img src={img} width="50" height="50"/>*/}
+                {/*<img src={require('../../../test.png')} width="50" height="50"/>*/}
             {/*<table id="issueStats" className="col-sm-4 no-gutter">
                 <tr>
                     <td>Id</td>
